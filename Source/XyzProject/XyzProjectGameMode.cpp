0e5bde7d63180846bf8906dc18b0a2eb867ca55d
User <user@not.exist>
2025-01-06 14:34:17 -0500

    Reorganises blueprints and inputs, fixes redirections

--- Source/XyzProject/XyzProjectGameMode.cpp
+++ Source/XyzProject/XyzProjectGameMode.cpp
@@ -7,9 +7,13 @@
 AXyzProjectGameMode::AXyzProjectGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Core/Characters/Blueprints/BP_ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Error, TEXT("AXyzProjectGameMode: PlayerPawnBPClass.Class was not found!"));
 	}
 }
